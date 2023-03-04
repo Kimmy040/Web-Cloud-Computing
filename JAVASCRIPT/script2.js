a4ac801cbd3d8456fc633522767f5a9d2e5f4791
Your Name <Kimmy4u@example.co>
2023-03-04 12:45:59 +0000

    Ajust some undefined variables

--- JAVASCRIPT/script2.js
+++ JAVASCRIPT/script2.js
@@ -5,14 +5,16 @@ function setMovie(data) {
   title.innerHTML = data.title;
   let plot = document.getElementById("plot");
   plot.innerHTML = data.plot_overview;
-  let genre = document.getElementById("genre");
-  genre.innerHTML = data.genre_names ? data.genre_names.map(genre_names => genre_names.name).join(", ") : "Undefined";
+  var genre = data.genre_names ? data.genre_names : "Undefined";
+  document.getElementById("genre").innerHTML = genre;
   let rating = document.getElementById("rating");
   rating.innerHTML = data.user_rating ? data.user_rating : "Undefined";
+  let critic_score = document.getElementById("critic_score");
+  critic_score.innerHTML = data.critic_score ? data.critic_score : "Undefined";
   let year = document.getElementById("year");
   year.innerHTML = data.year ? data.year : "Undefined";
-  let platform = document.getElementById("platform");
-  platform.innerHTML = data.sources; //Needs fix
+  //let platform = document.getElementById("platform");
+  //platform.innerHTML = data.sources; //Needs fix
   let duration = document.getElementById("duration");
   duration.innerHTML = data.runtime_minutes ? data.runtime_minutes : "Undefined";
   let type = document.getElementById("type");
@@ -25,21 +27,27 @@ function setMovie(data) {
   release_date.innerHTML = data.release_date ? data.release_date : "Undefined";
   let tv_show = document.getElementById("type");  
   tv_show.innerHTML = data.type ? data.type : "Undefined";
-  let runtime_minutes = document.getElementById("runtime_minutes");
-  runtime_minutes.innerHTML = data.runtime_minutes ? data.runtime_minutes : "Undefined";
-  let format = document.getElementById("format");
-  format.innerHTML = data.format ? data.format : "Undefined";
+  //let format = document.getElementById("format"); //Needs to connect to sources
+  //format.innerHTML = data.sources.format ? data.sources.format : "Undefined";
   
+  //Call the trailer function
   trailer(data);
+
 }
+
 
 
 function trailer(data){
   let trailer = document.getElementById("trailer");
   const splitUrl = data.trailer.split("v=")
   const youtubeId = splitUrl[1]
-  trailer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+  if (data.trailer == null){
+    trailer.innerHTML = "No trailer available"
+  } else {
+    trailer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+  } 
 }
+
 
 
 
