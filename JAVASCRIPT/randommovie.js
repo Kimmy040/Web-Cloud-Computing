05d6545c3b44ad148ebc71629eba034e92a5068f
mrcrchln <124362563+mrcrchln@users.noreply.github.com>
2023-03-07 15:15:21 +0000

    added detailed comments to js

--- JAVASCRIPT/randommovie.js
+++ JAVASCRIPT/randommovie.js
@@ -1,43 +1,58 @@
+// This function displays the search results on the page
 function randomSearchResults(randoms, str) {
+  // Get the container element and replace its children
   const container = document.getElementById('container');
   container.replaceChildren()
+
+  // Log the search results and metadata to the console
   console.log(randoms.results);
   console.log("META", randoms.meta);
+
+  // If there are no search results, display an error message
   if (randoms.results.length == 0) {
     let responseText = document.createElement("span");
     responseText.appendChild(document.createTextNode(`The movie ${str} does not exist in the database`));
     container.appendChild(responseText)
   } else {
+    // If there are search results, display up to 3 random ones
     let results = randoms.results;
     if (results.length > 3) {
       results = shuffle(results).slice(0, 3);
     }
+    // Loop through the search results and create HTML elements for each one
     for (let i = 0; i < results.length; i++) {
       let fig = document.createElement("figure");
       let img = document.createElement("img");
       let fc = document.createElement("figcaption");
+      // Set the image source and alt text
       img.src = randoms.results[i].image_url;
       img.alt = randoms.results[i].name;
+      // If the image fails to load, replace it with a default "missing poster" image
       img.onerror = function() {
         this.src = '../IMAGES/missingposter.png'; // replace with missing poster image
       };
+      // Set the text content of the caption to the movie name and year (if available)
       fc.textContent = randoms.results[i].name;
       if (randoms.results[i].year !== null) {
         fc.textContent += ` ${randoms.results[i].year}`;
       }
+      // Add the image and caption to the figure element
       fig.appendChild(img);
       fig.appendChild(fc);
-      // add event listener to redirect to movieDetails.html
+      // // Add an event listener to the figure element to redirect to the movieDetails page when clicked
       fig.addEventListener("click", () => {
         window.location.href = `movieDetails.html?id=${results[i].id}`;
       });
+      // Add the figure element to the container
       let rout = document.querySelector(".randomoutput");
       rout.insertAdjacentElement("afterbegin", fig);
     }
   }
+  // Clear the search input field
   document.querySelector("#search").value = "";
 }
 
+// This function shuffles an array using the Fisher-Yates shuffle algorithm
 function shuffle(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
@@ -50,14 +65,21 @@ function shuffle(array) {
   return array;
 }
 
+// This event listener runs when the page finishes loading
 document.addEventListener("DOMContentLoaded", () => {
 // List of 100 most common words in movie titles
 const wordsList = 'the,of,a,and,in,to,for,with,on,at,from,by,an,is,his,her,she,him,he,they,their,them,it,that,this,be,but,or,as,up,who,out,not,one,all,into,has,are,we,was,you,me,my,your,our,can,will,love,man,woman,day,night,life,world,time,way,home,heart,mind,soul,body,death,game,war,king,queen,prince,princess,power,money,city,country,school,house,family,friends,adventure,journey,story,tale,legend,mystery,thriller,horror,comedy,drama,action,romance,fantasy,science,fiction,space,magic,force,hero,villain,battle,quest,treasure';
+// Split the words list into an array of individual words
 const wordsArray = wordsList.split(',');
+// Generate a random index to select a word from the array
 const randomIndex = Math.floor(Math.random() * wordsArray.length);
-const randomWord = wordsArray[randomIndex];
+// Select a random word from the array
+const randomWord = wordsArray[randomIndex];$
+  // Construct the API url with the selected random word and the API key
   let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${APIKEY}&search_type=2&search_value=${randomWord}`;
+  // Log the API url to the console
   console.log(url);
+  // Fetch data from the Watchmode API using the constructed url
   fetch(url)
     .then(response => response.json())
     .then(randoms => {randomSearchResults(randoms)})
@@ -66,18 +88,27 @@ const randomWord = wordsArray[randomIndex];
     });
 });
 
+// This event listener runs when the page finishes loading. We do this to ensure that the page has finished loading
+// before attempting to manipulate the DOM and add an event listener to the shuffle button. 
 document.addEventListener("DOMContentLoaded", () => {
+  // Select the shuffle button and the container element from the DOM
   const shuffleButton = document.getElementById("shuffle-button");
   const container = document.getElementById("container");
-
+  // Add an event listener to the shuffle button
   shuffleButton.addEventListener("click", () => {
-    // List of 100 most common words in movie titles
+  // List of 100 most common words in movie titles
   const wordsList = 'the,of,a,and,in,to,for,with,on,at,from,by,an,is,his,her,she,him,he,they,their,them,it,that,this,be,but,or,as,up,who,out,not,one,all,into,has,are,we,was,you,me,my,your,our,can,will,love,man,woman,day,night,life,world,time,way,home,heart,mind,soul,body,death,game,war,king,queen,prince,princess,power,money,city,country,school,house,family,friends,adventure,journey,story,tale,legend,mystery,thriller,horror,comedy,drama,action,romance,fantasy,science,fiction,space,magic,force,hero,villain,battle,quest,treasure';
+  // Split the words list into an array of individual words
   const wordsArray = wordsList.split(',');
+  // Generate a random index to select a word from the array
   const randomIndex = Math.floor(Math.random() * wordsArray.length);
+  // Select a random word from the array
   const randomWord = wordsArray[randomIndex];
+  // Construct the API url with the selected random word and the API key
     let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${APIKEY}&search_type=2&search_value=${randomWord}`;
+    // Log the API url to the console
     console.log(url);
+    // Fetch data from the Watchmode API using the constructed url
     fetch(url)
       .then(response => response.json())
       .then(randoms => {randomSearchResults(randoms)})
--- JAVASCRIPT/search.js
+++ JAVASCRIPT/search.js
@@ -1,75 +1,115 @@
+// Assign the API key to a variable
 let APIKEY = "Ysrr6mfHWkD5IHRC6m2NpZpJED4djCB4HUuduA3f";
 
+// Function that displays search results in the container element
 function searchResults(content, str) {
+  // Select the container element from the DOM and clear its children
   const container = document.getElementById('container');
   container.replaceChildren()
+
+  // Log the search results and meta data to the console for debugging purposes
   console.log(content.results);
   console.log("META", content.meta);
+
+  // If no search results are returned, display a message indicating that the movie doesn't exist in the database
   if (content.results.length == 0) {
     let responseText = document.createElement("span");
     responseText.appendChild(document.createTextNode(`The movie ${str} does not exsist in the database`));
     container.appendChild(responseText)
-  } else if (content.results.length > 3) {
+  }
+  // If more than 3 search results are returned, display the first 3 results with image and captions
+  else if (content.results.length > 3) {
+    // Create figure, image, and caption elements
+    // First and therefore most relevant search result is displayed first when adding the search results to the container element
     for (let i = 2; i > -1; i--) {
       let fig = document.createElement("figure");
       let img = document.createElement("img");
       let fc = document.createElement("figcaption");
+
+      // Set the image source and alt attributes, and add an error handler to replace the image with a missing poster image if it fails to load
       img.src = content.results[i].image_url;
       img.alt = content.results[i].name;
       img.onerror = function() {
-        this.src = '../IMAGES/missingposter.png'; // replace with missing poster image
+        this.src = '../IMAGES/missingposter.png'; // Replace with missing poster image
       };
+
+      // Set the caption text to the movie name and year (if available)
       fc.textContent = content.results[i].name;
       if (content.results[i].year !== null) {
         fc.textContent += ` ${content.results[i].year}`;
       }
+
+      // Append the image and caption to the figure element, and add an event listener to redirect to the movie details page when clicked
       fig.appendChild(img);
       fig.appendChild(fc);
-      // add event listener to redirect to movieDetails.html
+      // Add event listener to redirect to movieDetails.html
       fig.addEventListener("click", () => {
         window.location.href = `movieDetails.html?id=${content.results[i].id}`;
       });
-      let out = document.querySelector(".output");
-      out.insertAdjacentElement("afterbegin", fig);
-    }
-  } else {
-    for (let i = (content.results.length-1); i > -1; i--) {
-      let fig = document.createElement("figure");
-      let img = document.createElement("img");
-      let fc = document.createElement("figcaption");
-      img.src = content.results[0].image_url;
-      img.alt = content.results[0].name;
-      img.onerror = function() {
-        this.src = '../IMAGES/missingposter.png'; // replace with missing poster image
-      };
-      fc.textContent = content.results[i].name;
-      if (content.results[0].year !== null) {
-        fc.textContent += ` ${content.results[0].year}`;
-      }
-      fig.appendChild(img);
-      fig.appendChild(fc);
-      // add event listener to redirect to movieDetails.html
-      fig.addEventListener("click", () => {
-        window.location.href = `movieDetails.html?id=${content.results[0].id}`;
-      });
+
+      // Select the output element from the DOM and insert the figure element at the beginning
       let out = document.querySelector(".output");
       out.insertAdjacentElement("afterbegin", fig);
     }
   }
+  // If less than 3 but more than 0 search results are returned, display the first i results with image and captions
+  else {
+    // First and therefore most relevant search result is displayed first when adding the search results to the container element
+    for (let i = (content.results.length-1); i > -1; i--) {
+      // Create figure, image, and caption elements
+      let fig = document.createElement("figure");
+      let img = document.createElement("img");
+      let fc = document.createElement("figcaption");
+
+      // Set the image source and alt attributes, and add an error handler to replace the image with a missing poster image if it fails to load
+      img.src = content.results[0].image_url;
+      img.alt = content.results[0].name;
+      img.onerror = function() {
+        this.src = '../IMAGES/missingposter.png'; // Replace with missing poster image
+      };
+
+      // Set the caption text to the movie name and year (if available)
+      fc.textContent = content.results[i].name;
+      if (content.results[0].year !== null) {
+        fc.textContent += ` ${content.results[0].year}`;
+      }
+      // Append the image and caption to the figure element, and add an event listener to redirect to the movie details page when clicked
+      fig.appendChild(img);
+      fig.appendChild(fc);
+      // Add event listener to redirect to movieDetails.html
+      fig.addEventListener("click", () => {
+        window.location.href = `movieDetails.html?id=${content.results[0].id}`;
+      });
+
+      // Select the output element from the DOM and insert the figure element at the beginning
+      let out = document.querySelector(".output");
+      out.insertAdjacentElement("afterbegin", fig);
+    }
+  }
+  // Clear the text that was previously entered in the input field
   document.querySelector("#search").value = "";
 }
 
+// Listen for the "DOMContentLoaded" event, which indicates that the initial HTML document has been completely loaded and parsed
 document.addEventListener("DOMContentLoaded", init);
+  // Define the "init" function that will be called when the "DOMContentLoaded" event is triggered
   function init() {
+    // Add an event listener to the search button
     document.getElementById("btnSearch").addEventListener("click", ev => {
-        ev.preventDefault(); //to stop the page from reloading
+        // Prevent the default form submission behavior, which would cause the page to reload
+        ev.preventDefault();
         let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${APIKEY}&search_type=1&search_value=`;
-        let str = document.getElementById("search").value.trim();
-        url = url.concat(str);
+        // Get the search query entered by the user, remove leading and trailing whitespace
+        let str = document.getElementById("search").value.trim(); 
+        // Append the search query to the API URL
+        url = url.concat(str); 
         console.log(url);
+        // Send a GET request to the Watchmode API with the constructed URL, and handle the response asynchronously
         fetch(url)
-          .then(response => response.json())
-          .then(content => {searchResults(content, str)})
+          // Convert the response to a JSON object
+          .then(response => response.json()) 
+          // Call the "searchResults" function with the search results and query string as arguments
+          .then(content => {searchResults(content, str)}) 
           .catch(err => {
             console.error(err);
           });
